6b958186de8c6f6b751f1e9407a1aa12da5e367b
zengzirui <659280752@qq.com>
2025-08-22 11:07:58 +0800

    gitignore debug2 and add pypi commit

--- nextera_py/__init__.py
+++ nextera_py/__init__.py
@@ -1,13 +1,12 @@
-# 方案2：延迟导入（在函数内部导入）
+
 good_bullins = None
 def lazy_import():
     global good_bullins
-    from new_python import good_bullins
-# 方案2：延迟导入（在函数内部导入）
+    from nextera_py import good_bullins
 modules = None
 def lazy_import():
     global modules
-    from new_python import modules
+    from nextera_py import modules
 __version__ = '0.0.1'
 __author__ = 'jason'
 __all__ = ['good_builtins', 'modules']
